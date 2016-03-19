c1c9e5b6783f40e12079dbfd9bcbbd54542c45cd
shishir-insane <shishir.insane@gmail.com>
2016-03-19 23:29:13 +0530

    search controller

--- src/main/java/com/cg/apps/ipaid/controller/ViewController.java
+++ src/main/java/com/cg/apps/ipaid/controller/ViewController.java
@@ -1,14 +1,29 @@
 package com.cg.apps.ipaid.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ViewController {
 
-	@RequestMapping(value = "/")
-    public ModelAndView uploadView() {
-       return new ModelAndView("index");
-    }
+	private static final Logger LOGGER = LoggerFactory.getLogger(ViewController.class);
+
+	@RequestMapping(value = "/getUserName")
+	public String getUserName() {
+		String username = null;
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+			username = ((UserDetails) principal).getUsername();
+		} else {
+			username = principal.toString();
+		}
+		LOGGER.info("UserName: {}", username);
+		return username;
+	}
+
+
 }
