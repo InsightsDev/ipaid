8337acb56fd2173932c7fcd4f9cd1349e8803490
strider90arun <strider90arun@gmail.com>
2016-03-19 11:33:44 +0530

    Mongo stuff

---
+++ src/main/java/com/cg/apps/ipaid/request/PurchaseRequest.java
@@ -0,0 +1,72 @@
+package com.cg.apps.ipaid.request;
+
+import java.io.File;
+
+public class PurchaseRequest {
+
+	private String productName;
+	private String invoiceNo;
+	private Double productCost;
+	private String productCode;
+	private String location;
+	private String storeName;
+	private String userId;
+	private String purchaseDate;
+	private File bill;
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getInvoiceNo() {
+		return invoiceNo;
+	}
+	public void setInvoiceNo(String invoiceNo) {
+		this.invoiceNo = invoiceNo;
+	}
+	public Double getProductCost() {
+		return productCost;
+	}
+	public void setProductCost(Double productCost) {
+		this.productCost = productCost;
+	}
+	public String getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(String purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	public File getBill() {
+		return bill;
+	}
+	public void setBill(File bill) {
+		this.bill = bill;
+	}
+	
+	
+}
